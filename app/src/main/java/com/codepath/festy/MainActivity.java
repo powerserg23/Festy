9f3239954f789e41c8b6efcb2587f1130bb8e0ed
Chase Chipres <chasechipres@gmail.com>
2020-02-01 18:31:33 -0800

    cleaned up some of old test code

--- app/src/main/java/com/codepath/festy/MainActivity.java
+++ app/src/main/java/com/codepath/festy/MainActivity.java
@@ -2,10 +2,6 @@ package com.codepath.festy;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,21 +21,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView rvArts;
-    List<Act> actData; //Artist+time+*add button*
-
-    Button mButtonTest;
-    TextView mConditionTextView;
+    List<Act> actData;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mFestivalRef = mRootRef.child("festival");
     DatabaseReference mCoachellaRef = mFestivalRef.child("0");
-    DatabaseReference mNameRef = mCoachellaRef.child("name");
     DatabaseReference mScheduleRef = mCoachellaRef.child("schedule");
-    DatabaseReference mPerformanceRef = mScheduleRef.child("0");
-    DatabaseReference mArtistRef = mPerformanceRef.child("artist");
-
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference("test");
 
     final String TAG = "MainActivity";
 
@@ -48,59 +35,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButtonTest = (Button)findViewById(R.id.buttonTest);
-        mConditionTextView = (TextView)findViewById(R.id.textViewTest);
-
-        rvArts=findViewById(R.id.rvArts);
-        actData=new ArrayList<>();
+        rvArts = findViewById(R.id.rvArts);
+        actData = new ArrayList<>();
 
         final ActAdapter actAdapter = new ActAdapter(this, actData);
         rvArts.setAdapter(actAdapter);
         rvArts.setLayoutManager(new LinearLayoutManager(this));
-        //JSONObject
-
     }
 
     protected void onStart() {
         super.onStart();
 
-        Log.d(TAG, "START" + mNameRef.toString());
-
         mScheduleRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                   for (DataSnapshot actSnapshot : dataSnapshot.getChildren()) {
-                                                                Act tempAct = new Act(actSnapshot.child("artist").getValue(String.class), actSnapshot.child("time").getValue(String.class), actSnapshot.child("stage").getValue(String.class));
-                                                                actData.add(tempAct);
-                                                            }
-                                                        }
-
-                                                        @Override
-                                                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                                        }
-
-                                                    });
-
-        mNameRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-                mConditionTextView.setText(text);
-                Log.d(TAG, "TESTING_LOG: " + dataSnapshot.toString());
+               for (DataSnapshot actSnapshot : dataSnapshot.getChildren()) {
+                    Act tempAct = new Act(actSnapshot.child("artist").getValue(String.class), actSnapshot.child("time").getValue(String.class), actSnapshot.child("stage").getValue(String.class));
+                    actData.add(tempAct);
+                    Log.d(TAG, "LOG- " + tempAct.getName());
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
 
-        mButtonTest.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mNameRef.setValue("Coachella2");
-            }
         });
 
     }
--- app/src/main/java/com/codepath/festy/models/Act.java
+++ app/src/main/java/com/codepath/festy/models/Act.java
@@ -1,36 +1,18 @@
 package com.codepath.festy.models;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.content.ContentValues.TAG;
-
 public class Act {
     String time;
     String name;
     String stage;
+
     public Act(){};
+
     public Act(String name,String time,String stage)
     {
-   this.time=time;
-   this.name=name;
-   this.stage=stage;
+       this.time=time;
+       this.name=name;
+       this.stage=stage;
     }
-
-
 
     public String getTime() {
         return time;
