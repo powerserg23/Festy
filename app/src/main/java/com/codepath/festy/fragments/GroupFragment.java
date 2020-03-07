c2663153cb91642ae231fbe0ac7f570c5f3be983
derekn4 <derekn4@uci.edu>
2020-03-06 18:26:38 -0800

    Add Blank Fragments for Group and Profile Pages

---
+++ app/src/main/java/com/codepath/festy/fragments/GroupFragment.java
@@ -0,0 +1,32 @@
+package com.codepath.festy.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.festy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroupFragment extends Fragment {
+
+
+    public GroupFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false);
+    }
+
+}
