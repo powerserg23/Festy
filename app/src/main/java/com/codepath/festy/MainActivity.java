bcfe52f3bbe6ced730f027ccb37fc70402a70ccb
derekn4 <derekn4@uci.edu>
2020-03-06 21:10:46 -0800

    Added outline for Profile Page

--- app/src/main/java/com/codepath/festy/MainActivity.java
+++ app/src/main/java/com/codepath/festy/MainActivity.java
@@ -5,56 +5,32 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.codepath.festy.fragments.GroupFragment;
 import com.codepath.festy.fragments.ListFragment;
-import com.codepath.festy.adapters.ActAdapter;
-import com.codepath.festy.models.Act;
+import com.codepath.festy.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    //RecyclerView rvArts;
-    //List<Act> actData;
-
-    //DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    //DatabaseReference mFestivalRef = mRootRef.child("festival");
-    //DatabaseReference mCoachellaRef = mFestivalRef.child("0");
-    //DatabaseReference mScheduleRef = mCoachellaRef.child("schedule");
 
     final String TAG = "MainActivity";
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
-    //SharedPreferences settings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //rvArts = findViewById(R.id.rvArts);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
-        //actData = new ArrayList<>();
 
-        //settings = getSharedPreferences("prefs", MODE_PRIVATE);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -66,12 +42,12 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.action_group:
                         Toast.makeText(MainActivity.this, "Group Page", Toast.LENGTH_SHORT).show();
-                        fragment = new ListFragment();
+                        fragment = new GroupFragment();
                         break;
                     case R.id.action_profile:
                     default:
                         Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                        fragment = new ListFragment();
+                        fragment = new ProfileFragment();
                         break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
--- app/src/main/java/com/codepath/festy/fragments/ProfileFragment.java
+++ app/src/main/java/com/codepath/festy/fragments/ProfileFragment.java
@@ -3,19 +3,36 @@ package com.codepath.festy.fragments;
 
 import android.os.Bundle;
 
+import android.util.Log;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.festy.R;
+import com.codepath.festy.adapters.ActAdapter;
+import com.codepath.festy.models.Act;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    public static final String TAG = "ProfileFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 54;
+    private TextView tvUsername;
+    private ImageView ivUserImage;
+    private Button btnUpload;
+    private TextView tvLabel;
+    private RecyclerView rvLineup;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -27,6 +44,25 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        ivUserImage = view.findViewById(R.id.ivUserImage);
+        btnUpload = view.findViewById(R.id.btnUpload);
+        tvLabel = view.findViewById(R.id.tvLabel);
+        rvLineup = view.findViewById(R.id.rvLineup);
+
+        List<Act> actData;
+
+        //TODO: Get all Artists that the user selected and display
+        //Step 1: get data from ListFragment
+        //Step 2: filter data for only the artists that users selected
+        //Step 3: put data into actData List
+        //Step 4: display actData into rvLineup
+        //Step 5: click button allows user to change profile picture (i.e change ivUserImage)
     }
 
 }
